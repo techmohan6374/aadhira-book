5b21e8384fa9331b0bf572fb5e99fc2ac40345f7
Mohanraj <mohanmano2020@gmail.com>
2024-08-14 23:25:55 +0530

    secondary commit

--- components/Main.js
+++ components/Main.js
@@ -2,11 +2,32 @@ const Main = {
     template: `
     <div id="mainPage">
        <div class="container-fluid p-0">
+            <header class="flex pt-4">
+                <h1>ஆதிரா புத்தகங்கள்</h1>
+            </header>
+            <div class="container">
+                <div class="row py-4">
+                    <div class="col-12 col-xl-6">
+                        <div class="card book-card">
+                            <div class="card-image">
+                                <img src="/resources/images/Books/1.jpg" alt="Book Cover" loading="lazy"/>
+                            </div>
+                            <div class="card-content mt-2">
+                                <h6>மகாபாரதம் பகவான் ஸ்ரீ கிருஷ்ணர்</h6>
+                                <button class="flex" v-on:click="openFlipBook(0)">
+                                    <iconify-icon icon="octicon:book-16"></iconify-icon>
+                                    பார்க்கவும்
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
        </div>
     </div>
     `,
     created() {
-      
+
     },
     data() {
         return {
@@ -14,6 +35,8 @@ const Main = {
         };
     },
     methods: {
-      
+        openFlipBook(id) {
+            this.$router.push(`/singleflipBook/${id}`);
+        },
     },
 };
--- components/singleflipBook.js
+++ components/singleflipBook.js
@@ -2,18 +2,30 @@ const SingleFlipBook = {
     template: `
     <div id="singleFlipBook">
        <div class="container-fluid p-0">
+            <div id="flipbookPDFContainer"></div>
        </div>
     </div>
     `,
     created() {
-      
+        this.index = this.$route.params.id;
+        console.log(this.index);
+        this.selectedPdfUrl = this.files[this.index];
+        console.log(this.selectedPdfUrl);
+        var source_pdf = this.selectedPdfUrl;
+        var option_pdf = { webgl: true, backgroundColor: "#363265" };
+        var flipBook_pdf = $("#flipbookPDFContainer").flipBook(
+            source_pdf,
+            option_pdf
+        );
     },
     data() {
         return {
-
+            files: ['/resources/pdf/Books/1.pdf'],
+            index: null,
+            selectedPdfUrl: "",
         };
     },
     methods: {
-      
+
     },
 };
